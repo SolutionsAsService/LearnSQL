3a6e4cbf10d814bf55377065e1cc211e2082f5ee
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-28 16:44:28 -0800

    Create insert_example.sql

---
+++ basic_operations/insert_example.sql
@@ -0,0 +1,7 @@
+-- This query demonstrates how to insert new records into a table.
+INSERT INTO users (first_name, last_name, age)
+VALUES ('John', 'Doe', 30);
+
+-- Explanation:
+-- `INSERT INTO` is used to add new rows to the specified table.
+-- You specify the columns and corresponding values to insert.
