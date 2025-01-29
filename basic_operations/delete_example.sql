4a306202edc56ed6f000879e1bb1196f9c61355d
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-28 16:46:58 -0800

    Create delete_example.sql

---
+++ basic_operations/delete_example.sql
@@ -0,0 +1,7 @@
+-- This query demonstrates how to delete records from a table.
+DELETE FROM users
+WHERE age < 20;
+
+-- Explanation:
+-- `DELETE FROM` removes records from the table.
+-- `WHERE` ensures only specific records are deleted.
